102ad0f44855415b683556a2269772d45410579f
squirtle shimadamada <tanderson0876@gmail.com>
2021-09-17 14:46:55 -0400

    name

---
+++ index.js
@@ -0,0 +1,20 @@
+// const sleep = (time) => {
+//   return new Promise((resolve) => setTimeout(resolve, time));
+// };
+// console.log("hello world");
+
+// const main = async () => {
+//   await sleep(1000);
+
+//   document.getElementById("something").innerHTML = "pog";
+// };
+
+// main();
+// async function funct1() {
+//   document.getElementById("something").innerHTML =
+//     "pog" + document.getElementById("something").innerText;
+//   await sleep(1000);
+//   funct1();
+// }
+
+// funct1();
